3f7689b8b25592a19776811d10c69eeb27b42f25
kinkenkan298 <akin@programmer.net>
2024-12-12 15:24:41 +0700

    inisialisasi lazynvim config

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    'bettervim/yugen.nvim',
+    lazy = false,
+    priority = 1000,
+  },
+
+  { 
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+  }
+}
